f1b2fd8a1b3abdef615f72085b12fac59beb1164
orman071 <orman071@gmail.com>
2016-03-11 13:21:35 +0400

    maybe last

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -25,7 +25,6 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -52,7 +51,6 @@ ROOT_URLCONF = 'ask.urls'
 
 WSGI_APPLICATION = 'ask.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
@@ -76,8 +74,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'))
+
+STATICFILES_FINDERS = ("django.contrib.staticfiles.finders.FileSystemFinder",
+                       "django.contrib.staticfiles.finders.AppDirectoriesFinder",)
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.core.paginator import Paginator
-from qa.models import Question
+from qa.models import Question, Answer
 
 
 def test(request, pk):
+    post = Question.objects.all()
+    post.order_by('-id')
+    limit = request.GET.get('limit', 10)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(post, limit)
+    page = paginator.page(page)
+    return render(request, 'post.html', {'post': post, 'paginator': paginator, 'page': page})
 
 
+def popular(request):
+    pop = Question.objects.all().filter('-rating')
+    limit = request.GET.get('limit', 10)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(pop, limit)
+    page = paginator.page(page)
+    return render(request, 'post.html', {'post': pop, 'paginator': paginator, 'page': page})
 
+
+def question(request, id):
+    qu = get_object_or_404(Question, pk=id)
+    an = Answer.objects.all()
+    return render(request, 'question.html', {'qu': qu, 'an': an})
