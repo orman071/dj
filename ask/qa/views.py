46b3966d8c286b5dd41a0f8b321d13b677136f2b
orman071 <orman071@gmail.com>
2016-03-11 12:26:15 +0400

    123

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from qa.models import Question
 
-def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+
+def test(request, pk):
+
+
 
