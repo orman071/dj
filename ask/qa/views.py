a020ff0503f20c69e5c944559614baeb9ce9c880
orman071 <orman071@gmail.com>
2016-03-11 17:20:41 +0400

    fcvfdgv

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from qa.models import Question, Answer
 
 
 def test(request):
     post = Question.objects.all()
     post.order_by('-id')
-    limit = request.GET.get('limit', 1)
-    page = request.GET.get('page', 1)
-    paginator = Paginator(post, limit)
-    page = paginator.page(page)
-    return render(request, 'post.html', {'post': post, 'paginator': paginator, 'page': page})
+    paginator = Paginator(post, 1)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        contacts = paginator.page(1)
+    except EmptyPage:
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, 'post.html', {'post': post, 'paginator': contacts, 'page': page})
 
 
 def popular(request):
