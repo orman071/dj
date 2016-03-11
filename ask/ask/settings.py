a621fd89952ae88d42304b9e24155234d2e4fe2c
orman071 <orman071@gmail.com>
2016-03-11 16:52:48 +0400

    fg

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -55,11 +55,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
+    
     }
 }
 
